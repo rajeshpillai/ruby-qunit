17f15d13f324c9e1b8d893673acd94b659a01eae
rajeshpillai <pillai.rajesh@gmail.com>
2021-12-22 12:45:33 +0530

    Refactor

--- tests/sample_test.rb
+++ tests/sample_test.rb
@@ -2,46 +2,45 @@ require_relative "../lib/qtest"
 require_relative "../lib/test_case"
 
 
-QTest.describe "some examples" do 
-  it "can pass" do 
-    # actual == expected
-    (1 + 1).should == 2
-  end
-
-  it "can fail" do
-    # (1 + 1).should == 3
-  end
-
-  it "negative testing" do
-    (1 + 1).should != 3
-  end
-  
-end
-
-# New rspec like syntax
-QTest.describe "RSpect like expectations" do 
-  it "can expect values" do 
-    # expect(1 + 1).to(eq(2))
-    expect(1 + 1).to eq 2
-  end
-
-  it "1 + 1 not equal 3" do 
-    expect(1 + 2).not_to eq(4)
-  end
-
-  it "different string doesn't match" do 
-    expect('ruby').not_to eq('rails')
-  end
-
-  it "same string matches" do 
-    expect('rails').to eq('rails')
-  end
-  
-
-end
-
 
 class AssertionTest < TestCase
+  QTest.describe "some examples" do 
+    it "can pass" do 
+      # actual == expected
+      (1 + 1).should == 2
+    end
+  
+    it "can fail" do
+      # (1 + 1).should == 3
+    end
+  
+    it "negative testing" do
+      (1 + 1).should != 3
+    end
+    
+  end
+  
+  # New rspec like syntax
+  QTest.describe "RSpect like expectations" do 
+    it "can expect values" do 
+      # expect(1 + 1).to(eq(2))
+      expect(1 + 1).to eq 2
+    end
+  
+    it "1 + 1 not equal 3" do 
+      expect(1 + 2).not_to eq(4)
+    end
+  
+    it "different string doesn't match" do 
+      expect('ruby').not_to eq('rails')
+    end
+  
+    it "same string matches" do 
+      expect('rails').to eq('rails')
+    end
+  end
+  
+  
   #macro methods ruby
   QTest.describe "AssertionTest"  do 
     it "1 + 1 = 2"  do 
@@ -49,9 +48,7 @@ class AssertionTest < TestCase
     end
     
   end
-end
 
-class MatchersTest
   #macro methods ruby
   QTest.describe "Matchers test"  do 
     it "Truthy"  do 
